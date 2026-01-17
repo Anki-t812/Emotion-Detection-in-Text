c12fc902e16569c857abf29504deb4a2a6dee710
Ankit <ankitrajaraj99@gmail.com>
2026-01-17 16:28:56 +0530

    lowrcase,url and spaces  removed

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -9,8 +9,12 @@ df = pd.read_csv(DATA_PATH)
 
 df=df.drop_duplicates()
 
-def remove_mention(text):
-  return re.sub(r'@\w+','',text)
+def clean_data(text):
+  text=text.lower()
+  text=re.sub(r'http\S+|www\S+','',text)#url remove no emotion
+  text=re.sub(r'@\w+','',text)#mentions remove no emotion
+  text = re.sub(r'\s+', ' ', text).strip()#extra spaces
+  return text
 
-df['content']=df['content'].apply(remove_mention)
+df['content']=df['content'].astype(str).apply(clean_data)
 df.to_csv("data/clean_emotions.csv", index=False)
--- src/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-tweet=pd.read_csv(r"C:\CODEX\Emotion Detection\data\tweet_emotions.csv")
-tweet.head()
