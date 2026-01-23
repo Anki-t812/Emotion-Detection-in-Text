6f7d82833f3509140ef5071c26c6819b58e0eb1c
Ankit <ankitrajaraj99@gmail.com>
2026-01-23 18:51:56 +0530

    deleted twitter_id column and split datat in 80/20 train_test

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH = BASE_DIR / "data" / "tweet_emotions.csv"
 
 df = pd.read_csv(DATA_PATH)
-
+df=df.drop(columns=['tweet_id'])
 df=df.drop_duplicates()
 
 def clean_data(text):
--- src/train_model.py
+++ src/train_model.py
@@ -1,9 +1,27 @@
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 BASE_DIR=Path(__file__).resolve().parent.parent
 DATA_PATH=BASE_DIR/"data"/"clean_emotions.csv"
 
 df=pd.read_csv(DATA_PATH)
-print(df.head())
-print(df['sentiment'].value_counts())
+
+X=df['content']
+y=df['sentiment']
+
+X_train,X_test,y_train,y_test=train_test_split(
+  X,                #input data
+  y,                # label or output
+  test_size=0.2,    # split size
+  stratify=y,       #same proportion on both test and train
+  random_state=42   #for consistency of train_test
+)
+print("Original distribution:")
+print(y.value_counts(normalize=True))
+
+print("\nTrain distribution:")
+print(y_train.value_counts(normalize=True))
+
+print("\nTest distribution:")
+print(y_test.value_counts(normalize=True))
