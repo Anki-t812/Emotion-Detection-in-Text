5f1547a081253a7e0618feb14ff455dce4148a3c
Ankit <ankitrajaraj99@gmail.com>
2026-01-29 15:22:02 +0530

    trained via logistic regression with OvR and saved the model with tfidf vector

--- src/train_model.py
+++ src/train_model.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
+import joblib
 
 BASE_DIR=Path(__file__).resolve().parent.parent
 DATA_PATH=BASE_DIR/"data"/"clean_emotions.csv"
@@ -9,10 +11,11 @@ DATA_PATH=BASE_DIR/"data"/"clean_emotions.csv"
 df=pd.read_csv(DATA_PATH)
 
 df = df.dropna(subset=['content', 'sentiment'])
-##Splitiing of dataset
+#load the data
 X=df['content']
 y=df['sentiment']
 
+#split the dataset
 X_train,X_test,y_train,y_test=train_test_split(
   X,                #input data
   y,                # label or output
@@ -20,7 +23,7 @@ X_train,X_test,y_train,y_test=train_test_split(
   stratify=y,       #same proportion on both test and train
   random_state=42   #for consistency of train_test
 )
-
+#TF-IDF
 vectorizer=TfidfVectorizer(
   max_features=10000,   # max words in the dic of model
   stop_words='english', # removes common words from language
@@ -29,4 +32,18 @@ vectorizer=TfidfVectorizer(
 X_train_tfidf=vectorizer.fit_transform(X_train)#learns vocab and calc TFIDF
 
 X_test_tfidf=vectorizer.transform(X_test)#use the vocab and ignore new words
-print(f"Matrix Shape: {X_train_tfidf.shape}")
+
+#Logictic regression with OvR
+model=LogisticRegression(
+  max_iter=1000,#total iteration for w
+  n_jobs=-1 #core used all
+)
+model.fit(X_train_tfidf, y_train)
+#        SAVE MODEL
+MODEL_DIR=BASE_DIR/"models"
+MODEL_DIR.mkdir(exist_ok=True) #creates models folder if not
+
+joblib.dump(model, MODEL_DIR / "logistic_model.pkl")
+joblib.dump(vectorizer, MODEL_DIR / "tfidf.pkl")
+
+print("Model and TF-IDF vectorizer saved successfully.")
